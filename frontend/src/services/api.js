143a703247f7dc373556080357bc0d7a21d203f1
Luiz Mendes <luiz.mendes@crazytechlabs.com.br>
2025-08-08 11:57:21 -0300

    ajuste timout api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
-    timeout: 15000,
+    timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
     },
